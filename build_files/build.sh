d165393f2e9374a5c97ad5c2e43ef73d76e03e59
MRJN <marijan@necoski.de>
2025-07-13 19:34:24 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,29 +3,34 @@
 set -ouex pipefail
 
 # Enable Copr
- dnf5 -y copr enable ublue-os/staging
+dnf5 -y copr enable ublue-os/staging
 
 # System
-dnf5 -y install polkit pipewire pipewire-pulseaudio wireplumber psmisc sddm
+dnf5 -y install polkit pipewire pipewire-pulseaudio wireplumber psmisc sddm libinput
 
 # Display
-dnf5 -y install weston wlroots libinput mesa-dri-drivers mesa-libGL \
-      xorg-x11-server-Xwayland mesa-libgbm xdg-desktop-portal-wlr dbus-x11
+dnf5 -y install \
+    mesa-libgbm \
+    mesa-libGL \
+    mesa-dri-drivers  \
+    xorg-x11-server-Xwayland \
+    dbus-x11 \
+    xdg-desktop-portal-wlr \
+    wlroots
 
- # Terminal
+# Terminal
 dnf5 -y install git tmux nvim mc git
 
 # Desktop
 dnf5 -y install hyprland swaybg swaylock
 
- # Apps
+# Apps
  dnf5 -y install kitty
 
- # Disable Copr
- dnf5 -y copr disable ublue-os/staging
+# Disable Copr
+dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
 systemctl enable sddm.service
-systemctl enable wireplumber.service
