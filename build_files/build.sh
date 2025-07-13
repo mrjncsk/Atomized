3c69c22b0e1a636f3c39541e6f6d6a2566e203ff
MRJN <marijan@necoski.de>
2025-07-13 18:23:30 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,22 +2,20 @@
 
 set -ouex pipefail
 
-### Install packages
+# Enable Copr
+ dnf5 -y copr enable ublue-os/staging
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+ # Terminal
+ dnf5 -y install git tmux nvim mc git
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+ # System
+ dnf5 -y install hyprland
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+ # Desktop
+ dnf5 -y install kitty
+
+ # Disable Copr
+ dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
