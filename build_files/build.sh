e8740d07ea21019413beba29c0fbd7d733f484df
MRJN <marijan@necoski.de>
2025-07-13 22:30:29 +0200

    ???

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ouex pipefail
 
-# to workarround gnupg error on build, gpg: Fatal: can't create directory '/root/.gnupg': No such file or directory
-mkdir -p /root/.gnupg
-
 # Enable Copr
 dnf5 -y copr enable ublue-os/staging
 
