ebdd762162460a7f7f81ab1dff5b90eb56698802
MRJN <marijan@necoski.de>
2025-07-13 19:10:34 +0200

    other wayland packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
  dnf5 -y install git tmux nvim mc git
 
  # System
- dnf5 -y install wayland weston wlroots libinput mesa-dri-drivers mesa-libGL mesa-libgbm mesa-libwayland-egl xdg-desktop-portal-wlr swaybg swaylock polkit pipewire pipewire-pulseaudio wireplumber xorg-xwayland psmisc dbus-x11 sddm
+ dnf5 -y install weston wlroots libinput mesa-dri-drivers mesa-libGL xorg-x11-server-Xwayland mesa-libgbm xdg-desktop-portal-wlr swaybg swaylock polkit pipewire pipewire-pulseaudio wireplumber xorg-xwayland psmisc dbus-x11 sddm
 
  # Desktop
  dnf5 -y install kitty
