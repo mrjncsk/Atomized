6ee3e609078596648d7da001545c60b5b16597f1
MRJN <marijan@necoski.de>
2025-07-17 17:50:43 +0200

    os-release, banners

--- build/system.sh
+++ build/system.sh
@@ -4,6 +4,20 @@ set -ouex pipefail
 rm /usr/share/wallpapers/*.*
 rm /etc/skel/.config/autostart/steam.desktop
 
+sed -i \
+    -e 's/^NAME=.*/NAME="hyprblue"/' \
+    -e 's/^ID=.*/ID=hyprblue/' \
+    -e 's/^PRETTY_NAME=.*/PRETTY_NAME="hyprblue Atomic OS"/' \
+    -e 's/^LOGO=.*/LOGO=hyprblue-logo-icon/' \
+    -e 's|^CPE_NAME=.*|CPE_NAME="cpe:/o:universal-blue:hyprblue:42"|' \
+    -e 's/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=hyprblue/' \
+    -e 's|^HOME_URL=.*|HOME_URL="https://github.com/mrjncsk/hyprblue"|' \
+    -e 's|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL="https://github.com/mrjncsk/hyprblue"|' \
+    -e 's|^SUPPORT_URL=.*|SUPPORT_URL="https://github.com/mrjncsk/hyprblue"|' \
+    -e 's|^BUG_REPORT_URL=.*|BUG_REPORT_URL="https://github.com/mrjncsk/hyprblue/issues"|' \
+    -e 's/^BOOTLOADER_NAME=.*/BOOTLOADER_NAME="hyprblue"/' \
+    /usr/lib/os-release
+
 # Enable Copr
 dnf5 -y copr enable ublue-os/staging
 dnf5 -y copr enable solopasha/hyprland
