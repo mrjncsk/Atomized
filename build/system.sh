72c53f781abe366ea2694bb1bc8c26bfd347bf0e
MRJN <marijan@necoski.de>
2025-07-28 22:45:40 +0200

    rm already avaiable

--- build/system.sh
+++ build/system.sh
@@ -26,18 +26,11 @@ dnf5 -y install \
 # Install required Software for Caeletia Shell
 # https://github.com/caelestia-dots/caelestia?tab=readme-ov-file
 dnf5 -y install \
-    xdg-desktop-portal-gtk \
-    wl-clipboard \
     bluez \
     inotify-tools \
-    wireplumber \
     trash-cli \
     foot \
     starship \
-    btop \
-    jq \
-    socat \
-    ImageMagick \
     adw-gtk3-theme \
     qt5ct \
     qt6ct \
