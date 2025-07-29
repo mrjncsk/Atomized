e7da1b036b558f8a608aa2ba73929e4eb7937307
MRJN <103059356+mrjncsk@users.noreply.github.com>
2025-07-29 15:02:53 +0000

    Update system.sh

--- build/system.sh
+++ build/system.sh
@@ -48,13 +48,7 @@ dnf5 -y install \
     python3-build python3-installer \
     python3-hatchling python3-hatch-vcs \
     pipewire-libs pipewire-devel \
-    libnotify \
-    qt5-qtwayland \
-    qt5-qtbase-gui \
-    qt5-qtx11extras \
-    xcb-util \
-    xcb-util-cursor\
-    libxcb
+    libnotify
 
 ### Setup Caelestia
 cd /tmp
