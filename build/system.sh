0e3376b4fed8430ffad770feade54d565783ca7f
MRJN <marijan@necoski.de>
2025-07-30 21:23:09 +0200

    rm hypr

--- build/system.sh
+++ build/system.sh
@@ -57,6 +57,7 @@ cd /tmp
 git clone --depth 1 https://github.com/caelestia-dots/caelestia.git
 git clone --depth 1 https://github.com/caelestia-dots/shell.git
 git clone --depth 1 https://github.com/caelestia-dots/cli.git
+git clone --depth 1 https://github.com/Vladimir-csp/app2unit.git
 wget --no-hsts https://github.com/marella/material-symbols/raw/refs/heads/main/material-symbols/material-symbols-rounded.woff2
 wget --no-hsts https://github.com/marella/material-symbols/raw/refs/heads/main/material-symbols/material-symbols-outlined.woff2
 wget --no-hsts https://github.com/marella/material-symbols/raw/refs/heads/main/material-symbols/material-symbols-sharp.woff2
@@ -65,7 +66,9 @@ cp -Rf /tmp/caelestia /etc/skel/.local/share/
 ln -s ../.local/share/caelestia/hypr /etc/skel/.config/hypr
 cp -Rf /tmp/shell /etc/skel/.config/quickshell/caelestia
 cp -Rf /tmp/material* /usr/share/fonts/material-symbols
-pip install --prefix=/usr app2unit
+
+### Install app2unit
+python3 -m pip install --no-user --prefix=/usr/ ./app2unit
 
 ### Build Caelestia Beat Detector
 g++ -std=c++17 -Wall -Wextra \
