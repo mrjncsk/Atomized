b3169eef9099352420a182a46276bf3fe106ad68
MRJN <marijan@necoski.de>
2025-08-06 20:28:14 +0200

    allow erase

--- Build/hyprpanel.sh
+++ Build/hyprpanel.sh
@@ -5,19 +5,41 @@ set -ouex pipefail
 dnf5 -y copr enable solopasha/hyprland
 dnf5 -y copr enable heus-sueh/packages
 
-### Remove conflicting Software
-dnf remove -y tuned tuned-ppd
-
 ### Install Software
-dnf5 -y install hyprland xdg-desktop-portal-hyprland hyprpanel thunar
-dnf5 -y install wireplumber upower libgtop2 bluez bluez-tools grimblast hyprpicker btop NetworkManager wl-clipboard swww brightnessctl gnome-bluetooth aylurs-gtk-shell power-profiles-daemon gvfs nodejs wf-recorder
-npm install -g sass
-dnf5 -y install python python3-pip
+dnf5 -y install \
+    hyprland \
+    hypridle \
+    hyprlock \
+    hyprpanel \
+    hyprshot \
+    hyprpicker \
+    hyprland-qtutils \
+    wofi \
+    matugen \
+    swww \
+    xdg-desktop-portal-hyprland \
+    thunar \
+    wireplumber \
+    upower \
+    libgtop2 \
+    bluez \
+    bluez-tools \
+    grimblast \
+    btop \
+    NetworkManager \
+    wl-clipboard \
+    brightnessctl \
+    gnome-bluetooth \
+    aylurs-gtk-shell \
+    power-profiles-daemon \
+    gvfs \
+    nodejs \
+    wf-recorder \
+    python python3-pip \
+    greetd tuigreet \
+    --allowerasing
 pip install gpustat pywal
-dnf5 -y install greetd tuigreet
-
-### Install Greeter
-#
+npm install -g sass
 
 ### Disable Copr
 dnf5 -y copr disable solopasha/hyprland
