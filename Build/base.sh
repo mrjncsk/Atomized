d9cb83e9197420c2c867f202f330a3ed37340ea8
MRJN <103059356+mrjncsk@users.noreply.github.com>
2025-08-07 12:35:17 +0000

    Update base.sh

--- Build/base.sh
+++ Build/base.sh
@@ -17,7 +17,6 @@ dnf5 -y install \
     xorg-x11-drv-ati \
     xorg-x11-drv-intel \
     xorg-x11-drv-nouveau \
-    xorg-x11-drv-vesa \
     xorg-x11-drv-vmware \
     glx-utils \
     kitty nvim mc \
