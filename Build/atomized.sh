4902edf73e11a38df9777d325449d56867b12e32
MRJN <marijan@necoski.de>
2025-08-09 14:04:18 +0200

    Update atomized.sh

--- Build/atomized.sh
+++ Build/atomized.sh
@@ -4,11 +4,14 @@ set -ouex pipefail
 dnf5 -y install \
         mesa-vulkan-drivers \
         mesa-dri-drivers \
+        plasma-breeze \
         libglvnd \
         fastfetch \
         cmatrix \
+        zoxide \
         kitty \
         btop \
         nvim \
         tmux \
+        lsd \
         mc
